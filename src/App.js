06f3fc1ac7da4ab3da043db8ec0a12776948ec65
Harris-Butt <33513710+Harris-Butt@users.noreply.github.com>
2020-06-06 23:38:20 +0500

    Assignment 003

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  Hello  from './Hello.js';
 
-function App() {
+
+function App({name,age}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>Hello <strong>Haris</strong>
+    <h2>Some frameWorks</h2>
+    <ol>
+      <li>React </li>
+      <li>Angular </li>
+      <li>Vue </li>
+    </ol>
+    <h2>Some Editors</h2>
+    <ul>
+      <li className="green">Notepad</li>
+      <li>Atom</li>
+    </ul>
+    <h2>Lets do Some Math in react</h2>
+  <p> 2+3 = {2+3} </p>
+  </div>
+
   );
 }
 
---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function Hello({firstName}) {
+return <p>This is paragraph component with {firstName}</p>
+}
+
+export default Hello;
